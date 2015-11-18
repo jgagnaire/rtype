67bf7b0c096a189d0db5e9ab51ff1d3458daf437
TAWFIK Emile <emile.tawfik@gmail.com>
2015-11-18 16:03:18 +0100

    Finish UpdSocket and TcpSocket

---
+++ client/inc/IUdpSocket.hh
@@ -0,0 +1,21 @@
+#ifndef IUDPSOCKET_HH_
+# define IUDPSOCKET_HH_
+
+# include <cstddef>
+# include <string>
+
+class IUdpSocket
+{
+    public:
+        IUdpSocket() {}
+        virtual ~IUdpSocket() {}
+
+        virtual bool            bind(unsigned short int port) = 0;
+        virtual void            unbind() = 0;
+        virtual std::size_t     send(const void *buf, std::size_t size,
+                const std::string &ip, unsigned short int port) = 0;
+        virtual std::size_t receive(void *buf, std::size_t size,
+                const std::string &ip, unsigned short &port) = 0;
+};
+
+#endif /* ifndef IUDPSOCKET_HH_ */
