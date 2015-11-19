aa031c3ad3ec5c8874880da0cf508da02a0439bc
alocatelli <alex.locatelli@epitech.eu>
2015-11-19 14:03:38 +0100

    Add Entity

--- client/inc/Compenent/ICompenent.hh
+++ client/inc/Compenent/ICompenent.hh
@@ -7,15 +7,21 @@
 class ICompenent
 {
 public:
-  ICompenent(const std::string& type);
+  ICompenent(const std::string& type, const std::string& name);
   virtual ~ICompenent();
 
   inline std::string typename(void) const {
     return (_type);
   }
+
+  inline std::string getName(void) const {
+    return (_name);
+  }
+  
   virtual void update(sf::Time fps) = 0;
 protected:
   std::string	_type;
+  std::string	_name;
 };
 
 #endif //ICOMPENENT_H_
---
+++ client/inc/Entity/Entity.hh
@@ -0,0 +1,17 @@
+#ifndef ENTITY_H_
+# define ENTITY_H_
+
+#include "CompenentManager.hh"
+
+class Entity
+{
+public:
+  Entity() {}
+  ~Entity() {}
+
+  void			update() {}
+
+  CompenentManager	manager;
+}
+
+#endif //ENTITY_H_
