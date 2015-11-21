1174e552b6372b966d64fef0a98799349b152d19
TAWFIK Emile <emile.tawfik@gmail.com>
2015-11-21 17:18:02 +0100

    add test for tcp send

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,15 +3,35 @@
 #include "UdpSocket.hh"
 #include "TcpSocket.hh"
 
-int main()
+int main(int ac, char **av)
 {
     NetworkManager nm;
     UdpPacket tosend;
+    TcpPacket tosend2;
 
-    tosend.setID(1);
-    tosend.setData(const_cast<char*>("coucou"));
-    tosend.setSize(6);
-    tosend.setQuery(2);
-    nm.send(tosend);
+    if (ac == 1)
+    {
+        std::cout << "choose 1: Udp, 2: Tcp" << std::endl;
+        return (0);
+    }
+    if (av[1][0] == '1')
+    {
+        std::cout << "Sending a udp packet like this {6, 2, 1, \"coucou\"}"
+            << " 127.0.0.1 4445" << std::endl;
+        tosend.setID(1);
+        tosend.setData(const_cast<char*>("coucou"));
+        tosend.setSize(6);
+        tosend.setQuery(2);
+        nm.send(tosend);
+    }
+    else if (av[1][0] == '2')
+    {
+        std::cout << "Connect and Sending a tcp packet like this {6, 2, \"coucou\"}"
+            << " 127.0.0.1 4443" << std::endl;
+        tosend2.setData(const_cast<char*>("coucou"));
+        tosend2.setSize(6);
+        tosend2.setQuery(2);
+        nm.send(tosend2);
+    }
     return 0;
 }
