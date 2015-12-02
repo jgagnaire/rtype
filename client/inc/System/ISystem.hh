fe15fac1ee8e97fb5aa8f9c57d2e5c58835c3a9a
alocatelli <alex.locatelli@epitech.eu>
2015-12-02 14:17:08 +0100

    add ISystem

--- client/inc/System/ISystem.hh
+++ client/inc/System/ISystem.hh
@@ -5,11 +5,16 @@
 
 class ISystem
 {
+public:
   ISystem() {}
   virtual ~ISystem() {}
 
   virtual void update(std::chrono::steady_clock) = 0;
   virtual void *out(void *) = 0; //remplacer le void* par un IPacket* quand le gros lard l'aura fait
+  virtual bool handle(REvent e) = 0;
+  virtual std::vector<REvent> broadcast(void) = 0;
+protected:
+  std::vector<Revent> 
 };
 
 #endif //ISYSTEM_HH_
