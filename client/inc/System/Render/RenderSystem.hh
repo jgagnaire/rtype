f2b74f01ef741f549af38dbcc8ef3eadc3953bad
TAWFIK Emile <emile.tawfik@gmail.com>
2015-12-04 16:58:58 +0100

    window preparation

--- client/inc/System/Render/RenderSystem.hh
+++ client/inc/System/Render/RenderSystem.hh
@@ -4,7 +4,7 @@
 # include "System/ISystem.hh"
 # include "System/Render/MenuScene.hh"
 # include "System/Render/StageScene.hh"
-
+# include "Window.hh"
 
 class RenderSystem : public ISystem
 {
@@ -17,11 +17,14 @@ class RenderSystem : public ISystem
         virtual bool handle(REvent e);
         virtual std::vector<REvent> broadcast(void);
 
+        inline IWindow     *getWindow() const;
+
     private:
         std::chrono::time_point<std::chrono::steady_clock>  _last;
         Scene                                               *_current;
         MenuScene                                           _menu;
         StageScene                                          _stage;
+        IWindow                                             *_window;
 };
 
 #endif /* end of include guard: RENDERSYSTEM_HH_C8GDRQJ2 */
