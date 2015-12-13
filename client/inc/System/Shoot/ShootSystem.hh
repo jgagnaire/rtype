4d5a73e73d670ecb9f983eedfdd2611d57f89941
alocatelli <alex.locatelli@epitech.eu>
2015-12-13 15:08:30 +0100

    add working shoot

--- client/inc/System/Shoot/ShootSystem.hh
+++ client/inc/System/Shoot/ShootSystem.hh
@@ -44,36 +44,32 @@ public:
 						get<std::pair<float, float> >("position");
 					if (tmp.first > 1920 || tmp.second > 1080
 						|| tmp.first < 0 || tmp.second < 0)
-					{
-						std::cout << _eList->size() << std::endl;
-						_eList->erase(x);
-						//delete x;
-						std::cout << _eList->size() << std::endl;
-						std::cout << "LES FESSES CA DELETE" << std::endl;
-					}
+						x = _eList->erase(x);
 				}
 			}
-			std::cout << "Bye bye" << std::endl;
 		}
 	
 	virtual IPacket                 *out() {/*envoi de packet quand on tire*/ return NULL; }
 	virtual void                    in(IPacket*) {}
 	virtual bool                    handle(EventSum e)
 		{
-			if (e == Key_Fire)
+			if (e & Key_Fire)
 			{
 				Entity *e = new Entity;
 				
 				e->manager.add<std::string>("name", "playerShoot");
 				e->manager.add<std::string>("type", "shoot");
-				e->manager.add("velocity", 1.35f);
+				e->manager.add("velocity", 5.50f);
 				e->manager.add<size_t>("dammage", 25);  
 				for(auto x : *_eList)
 					if (x->manager.get<std::string>("name") == "player1")
 					{
 						e->manager.add("position",
-									   x->manager.
-									   get<std::pair<float, float> >("position"));
+									   std::pair<float, float>(
+										   x->manager.get<std::pair<float, float> >
+										   ("position").first + 105.0f,
+										   x->manager.get<std::pair<float, float> >
+										   ("position").second + 9.0f));
 					}
 				e->manager.add<Pattern::Side>("direction", Pattern::Side::RIGHT);
 				e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
