678fe6cc9023d854e6d715007e5b199c711b25a5
alocatelli <alex.locatelli@epitech.eu>
2015-12-13 13:07:05 +0100

    complete shoot system

--- client/inc/System/Shoot/ShootSystem.hh
+++ client/inc/System/Shoot/ShootSystem.hh
@@ -10,9 +10,9 @@ namespace Pattern {
 	
 	void	line(Entity &e, Side s, int duration)
 	{
-		std::pair<float, float> pos = e.manager.get("position");
-		float vel = e.manager.get("velocity");
-		if (s == LEFT)
+		std::pair<float, float> pos = e.manager.get<std::pair<float, float> >("position");
+		float vel = duration + e.manager.get<float>("velocity");
+		if (s == Side::LEFT)
 			vel *= -1;
 		pos.second += vel;
 		e.manager.set("position", pos);
@@ -27,17 +27,25 @@ public:
 	
 	virtual void                    update(int duration)
 		{
-			for(auto x : _eList)
+			for(auto x : *_eList)
 			{
 				if (x->manager.get<std::string>("type") == "shoot")
 				{
-					/*nique ta mere*/
+					x->manager.get
+						<void (Entity&, Pattern::Side, int)>
+						("pattern")(*x, x->manager.
+									get<Pattern::Side>("direction"), duration);
+					std::pair<float, float> tmp = x->manager.
+						get<std::pair<float, float> >("position");
+					if (tmp.first > 1920 || tmp.second > 1080
+						|| tmp.first < 0 || tmp.second < 0)
+						delete x;
 				}
 			}
 		}
 	
-	virtual IPacket                 *out() {/*envoi de packet quand on tire*/}
-	virtual void                    in(IPacket*) { return NULL }
+	virtual IPacket                 *out() {/*envoi de packet quand on tire*/ return NULL; }
+	virtual void                    in(IPacket*) {}
 	virtual bool                    handle(EventSum e)
 		{
 			if (e == Key_Fire)
@@ -47,6 +55,7 @@ public:
 				e->manager.add("name", "playerShoot");
 				e->manager.add("type", "shoot");
 				e->manager.add("velocity", 1.35f);
+				e->manager.add<size_t>("dammage", 25);  
 				for(auto x : *_eList)
 					if (x->manager.get<std::string>("name") == "player1")
 					{
@@ -54,11 +63,12 @@ public:
 									   x->manager.
 									   get<std::pair<float, float> >("position"));
 					}
-				e->manager.add<Pattern::Side>("direction", Pattern::RIGHT);
+				e->manager.add<Pattern::Side>("direction", Pattern::Side::RIGHT);
 				e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
 					("pattern", Pattern::line);
-				_eList.push_back(e);
+				_eList->push_back(e);
 			}
+			return true;
 		}
 	virtual std::vector<REvent>     &broadcast(void)
 		{
