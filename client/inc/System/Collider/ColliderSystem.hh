338c13ec9e3fdd31035a109e0b9328c31a303b4f
TAWFIK Emile <emile.tawfik@gmail.com>
2015-12-18 14:54:35 +0100

    to test

--- client/inc/System/Collider/ColliderSystem.hh
+++ client/inc/System/Collider/ColliderSystem.hh
@@ -49,6 +49,7 @@ class ColliderSystem : public ASystem
 
         virtual void                    update(int)
         {
+            return ;
             std::pair<float, float> p1;
             std::pair<float, float> p2;
             std::pair<int, int>     s1;
@@ -60,7 +61,6 @@ class ColliderSystem : public ASystem
             {
                 for (auto a : *_eList)
                 {
-                    std::cout << "FIRST " << std::endl;
                     p1 = a->manager.get<std::pair<float, float> >("position");
                     t1 = a->manager.get<std::string>("type");
                     getSize(s1, t1, a->manager.get<std::string>("name"));
@@ -68,7 +68,6 @@ class ColliderSystem : public ASystem
                     {
                         if (a != b)
                         {
-                            std::cout << "SECONd " << t1 << " " << t2 << std::endl;
                             p2 = b->manager.get<std::pair<float, float> >("position");
                             t2 = b->manager.get<std::string>("type");
                             getSize(s2, t2, b->manager.get<std::string>("name"));
