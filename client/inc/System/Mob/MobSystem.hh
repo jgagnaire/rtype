0e5f4774de0295a1c2227879cd31d8e33a850def
TAWFIK Emile <emile.tawfik@gmail.com>
2015-12-20 11:46:44 +0100

    json modif

--- client/inc/System/Mob/MobSystem.hh
+++ client/inc/System/Mob/MobSystem.hh
@@ -12,28 +12,34 @@ class	MobSystem : public ASystem
             Entity *e = new Entity;
             e->manager.add<std::string>("name", name);
             e->manager.add<std::string>("type", "mob");
-            // TODO REMOVE THE IFS
-            if (_monsters.size())
-                e->manager.add<float>("velocity", _monsters[name].manager.get<float>("velocity"));
-            else
-                e->manager.add<float>("velocity", 2.0f);
+            e->manager.add<float>("velocity", _monsters[name].manager.get<float>("velocity"));
             e->manager.add<std::pair<float, float> >("position", pos);
-            if (_monsters.size())
-                e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
-                    ("pattern",
-                     Pattern::getPattern(
-                         _monsters[name].manager.get<std::string>("movement")));
-            else
-                e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
-                    ("pattern", &Pattern::line);
+            e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
+                ("pattern",
+                 Pattern::getPattern(
+                     _monsters[name].manager.get<std::string>("movement")));
             e->manager.add<Pattern::Side>("direction", Pattern::Side::LEFT);
             return e;
         }
+
+        Entity *createBonus(const std::string &name, const std::pair<float, float> &pos)
+        {
+            Entity *e = new Entity;
+            e->manager.add<std::string>("name", name);
+            e->manager.add<std::string>("type", "bonus");
+            e->manager.add<float>("velocity", _bonuses[name].manager.get<float>("velocity"));
+            e->manager.add<std::pair<float, float> >("position", pos);
+            e->manager.add<std::function<void (Entity&, Pattern::Side, int)> >
+                ("pattern",
+                 Pattern::getPattern(
+                     _bonuses[name].manager.get<std::string>("movement")));
+            e->manager.add<Pattern::Side>("direction", Pattern::Side::LEFT);
+            return e;
+        }
+
     public:
         MobSystem() {}
         MobSystem(std::list<Entity*> *list) : isActiv(false), _eList(list) {
-            std::pair<float, float> p(1920, 500);
-            _eList->push_back(createMob("mob2", p));
             _eventList.push_back(E_Stage);
         }
         virtual ~MobSystem() {}
@@ -80,6 +86,12 @@ class	MobSystem : public ASystem
                     for (auto &x : e.manager.getAll<Entity>())
                         _monsters[x.first] = x.second;
                 }
+                else if (p->getQuery() == static_cast<uint16_t>(Codes::JsonBonuses))
+                {
+                    Entity &e = JSONParser::parse(tmp)->getEntity().manager.get<Entity>("bonuses");
+                    for (auto &x : e.manager.getAll<Entity>())
+                        _bonuses[x.first] = x.second;
+                }
                 else if (p->getQuery() == static_cast<uint16_t>(Codes::JsonLevels))
                 {
                     Entity &e = JSONParser::parse(tmp)->getEntity().manager.get<Entity>("levels");
@@ -105,6 +117,29 @@ class	MobSystem : public ASystem
                                 for (int i = 0; i < numbers; ++i)
                                 {
                                     tmp = createMob(monster.first, pair);
+                                    tmp->manager.add<int>("appearIn", firstTime * 1000 + interval * i);
+                                    l.push_back(tmp);
+                                }
+                            }
+                        }
+                        for (auto &bonuses: main.second.manager.get<std::vector<Entity> >("bonuses"))
+                        {
+                            for (auto &bonus: bonuses.manager.getAll<Entity>())
+                            {
+                                int     numbers;
+                                int     firstTime;
+                                int     interval;
+
+                                Entity *tmp;
+                                Entity &pos = bonus.second.manager.get<Entity>("position");
+                                std::pair<float, float> pair(pos.manager.get<int>("x"),
+                                        pos.manager.get<int>("y"));
+                                numbers = bonus.second.manager.get<int>("time");
+                                firstTime = bonus.second.manager.get<int>("appear_at_sec");
+                                interval = bonus.second.manager.get<int>("timeval");
+                                for (int i = 0; i < numbers; ++i)
+                                {
+                                    tmp = createBonus(bonus.first, pair);
                                     tmp->manager.add<int>("appearIn", firstTime * 1000 + interval * i);
                                     l.push_back(tmp);
                                 }
