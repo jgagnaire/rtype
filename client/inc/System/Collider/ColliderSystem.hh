d94434d910801539f50b37d6c461eb6015827c0d
TAWFIK Emile <emile.tawfik@gmail.com>
2015-12-20 16:36:43 +0100

    explosion fix

--- client/inc/System/Collider/ColliderSystem.hh
+++ client/inc/System/Collider/ColliderSystem.hh
@@ -27,13 +27,14 @@ class ColliderSystem : public ASystem
                 ("pattern", Pattern::line);
             e->manager.add<Pattern::Side>("direction", Pattern::Side::LEFT);
             e->manager.add<fCollision>("collision", &Collision::explosion);
+            _event = E_Explosion;
             return e;
         }
 
         void                            setSize(std::pair<int, int> &s,
                 const std::string &name)
         {
-           if (_hitboxes.find(name) != _hitboxes.end())
+            if (_hitboxes.find(name) != _hitboxes.end())
             {
                 s.first = _hitboxes[name].first;
                 s.second = _hitboxes[name].second;
@@ -50,36 +51,40 @@ class ColliderSystem : public ASystem
             std::pair<float, float> p1, p2;
             std::pair<int, int>     s1, s2;
             std::string             t1, t2;
-			Pattern::Side			d1, d2;
-			bool					has_been_del = false;
+            Pattern::Side			d1, d2;
+            bool					has_been_del = false;
 
             if (_isActiv)
             {
                 for (auto a = _eList->begin(); a != _eList->end();)
                 {
-					has_been_del = false;
+                    has_been_del = false;
                     p1 = (*a)->manager.get<std::pair<float, float> >("position");
-					d1 = (*a)->manager.get<Pattern::Side>("direction");
+                    d1 = (*a)->manager.get<Pattern::Side>("direction");
                     setSize(s1, (*a)->manager.get<std::string>("name"));
-					for (auto b = _eList->begin(); b != _eList->end(); ++b)
+                    for (auto b = _eList->begin(); b != _eList->end(); ++b)
                     {
-						if (*a != *b)
+                        if (*a != *b)
                         {
                             p2 = (*b)->manager.get<std::pair<float, float> >("position");
                             t2 = (*b)->manager.get<std::string>("type");
-							d2 = (*b)->manager.get<Pattern::Side>("direction");
+                            d2 = (*b)->manager.get<Pattern::Side>("direction");
                             setSize(s2, (*b)->manager.get<std::string>("name"));
                             if (t1 != t2 && d1 != d2 && p1.first < p2.first + s2.first &&
                                     p1.first + s1.first > p2.first &&
                                     p1.second < p2.second + s2.second &&
                                     s1.second + p1.second > p2.second)
                             {
-                                bool delA = (*a)->manager.get<fCollision>("collision")(**a, **b);
-                                bool delB = (*b)->manager.get<fCollision>("collision")(**b, **a);
+                                std::pair<float, float> ex(-1, -1);
+                                bool delA = (*a)->manager.get<fCollision>("collision")(**a, **b, ex);
+                                if (ex.first > -1)
+                                    _eList->push_back(createExplosion(ex));
+                                ex.first = ex.second = -1;
+                                bool delB = (*b)->manager.get<fCollision>("collision")(**b, **a, ex);
+                                if (ex.first > -1)
+                                    _eList->push_back(createExplosion(ex));
                                 if (delA)
                                 {
-                                    _eList->push_back(createExplosion(p2));
-									_event = E_Explosion;
                                     a = _eList->erase(a);
                                     has_been_del = true;
                                 }
@@ -127,21 +132,21 @@ class ColliderSystem : public ASystem
         virtual std::vector<REvent>     &broadcast(void) { return _eventList; }
 
         virtual EventSum                getEvent()
-		{
-			    EventSum          tmp = 0;
+        {
+            EventSum          tmp = 0;
 
-				if (_event != noEvent)
-				{
-					tmp = _event;
-					_event = noEvent;
-				}
-				return tmp;
-		}
+            if (_event != noEvent)
+            {
+                tmp = _event;
+                _event = noEvent;
+            }
+            return tmp;
+        }
     private:
         bool	                                                    _isActiv;
         std::list<Entity*>                                          *_eList;
         std::unordered_map<std::string, std::pair<int, int> >       _hitboxes;
-	EventSum														_event;
+        EventSum														_event;
 
 };
 
