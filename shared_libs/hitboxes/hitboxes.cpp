1d34942c2e2be701c80a44d594a902a0726451e0
gagnai_j <gagnai_j@epitech.eu>
2015-12-23 17:45:55 +0100

    added 5 shared libs

---
+++ shared_libs/hitboxes/hitboxes.cpp
@@ -0,0 +1,26 @@
+#include "Entity/Entity.hh"
+#include "JSONParser.hh"
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64)|| defined(_WIN64)
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HINSTANCE, DWORD, LPVOID)
+{
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport)
+#else
+extern "C"
+{
+#endif
+Entity *dl_entry_point(void)
+{
+  try {
+    JSONParser::parseFile("entities/hitboxes.json");
+    return (new Entity(JSONParser::parse()->getEntity()));
+  }
+  catch(JSONException const &) { return (0); }
+}
+#if !defined(WIN32) && !defined(_WIN32) && !defined(WIN64) && !defined(_WIN64)
+}
+#endif
